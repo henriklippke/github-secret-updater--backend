63625cc5cad53f5114d2967342724d6be5cf4b02
Muhammad Hamad Awan <hammadawan0007@gmail.com>
2024-05-23 01:51:52 +0500

    Setup simple server

---
+++ index.js
@@ -0,0 +1,34 @@
+import { OAuthApp } from "octokit";
+import express from 'express';
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+app.use(express.json());
+app.use(cors());
+
+app.post('/access-token', async (req, res) => {
+  const clientId = process.env.CLIENT_ID
+  const clientSecret = process.env.CLIENT_SECRET
+
+  const oAuthApp = new OAuthApp({
+    clientId,
+    clientSecret,
+    defaultScopes: ["repo", "gist", "user", "project", "admin:org"]
+  });
+
+  const { code, state, accessToken } = req.body
+  try {
+    const token = await oAuthApp.createToken({ code, state })
+    res.json(token)
+  } catch (error) {
+    const token = await oAuthApp.resetToken({ token: accessToken })
+    res.json(token)
+  }
+});
+
+app.listen(PORT, () => {
+  console.log('Server is listenin on PORT :' + PORT);
+})
